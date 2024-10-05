9524e59a7e6f557fc87836dfc3ba157fb0c36abe
podrybaum <podrybaum@gmail.com>
2024-10-05 18:05:10 -0500

    Annotations finished.

--- button.lua
+++ button.lua
@@ -1,3 +1,4 @@
+--- Object representing a digital pushbutton controller input.
 ---@class Button
 ---@field new function
 ---@field controller Controller
@@ -17,6 +18,9 @@ Button.__tostring = function(self)
     return string.format("Button: %s", self.id)
 end
 
+--- Initialize a new Button instance.
+---@param controller Controller @A Controller instance.
+---@param id string @A unique identifier for the input.
 function Button.new(controller, id)
     local self = setmetatable({}, Button)
     self.controller = controller
@@ -30,6 +34,7 @@ function Button.new(controller, id)
     return self
 end
 
+--- Retrieves the state of the input.
 function Button:getState()
     local state = self.controller:xcGetRegValue(string.format("mcX360_LUA/%s", self.id))
     if type(state) ~= "number" then
@@ -57,54 +62,65 @@ function Button:getState()
     end
 end
 
+--- Create the properties panel UI for the input.
+---@param propertiesPanel any @A wxPanel object.
 function Button:initUi(propertiesPanel)
     local propSizer = propertiesPanel:GetSizer()
 
-    -- Slot labels and dropdowns
-    local options = {""}
-    local analogOptions = {""}
-    for _, slot in ipairs(self.controller.slots) do
-        options[#options + 1] = slot.id
-    end
-    local idMapping = {}
-    for i, signal in ipairs(self.signals) do
-        local label = wx.wxStaticText(propertiesPanel, wx.wxID_ANY, string.format("%s Action:", signal.id))
-        propSizer:Add(label, 0, wx.wxALIGN_LEFT + wx.wxALL, 5)
-        local choice = wx.wxChoice(propertiesPanel, wx.wxID_ANY, wx.wxDefaultPosition, wx.wxDefaultSize,
-            self.__type == "Trigger" and analogOptions or options)
-        idMapping[self.signals[i]] = choice
-        if self.signals[i].slot ~= nil then
-            choice:SetSelection(choice:FindString(self.signals[i].slot.id))
+    if not self == self.controller.shiftButton then
+        -- Slot labels and dropdowns
+        local options = {""}
+        local analogOptions = {""}
+        for _, slot in ipairs(self.controller.slots) do
+            options[#options + 1] = slot.id
         end
-        propSizer:Add(choice, 1, wx.wxEXPAND + wx.wxALL, 5)
-    end
-
-    -- Add the apply button and the event handler 
-    propSizer:Add(0, 0)
-    local applyId = wx.wxNewId()
-    local apply = wx.wxButton(propertiesPanel, applyId, "Apply", wx.wxDefaultPosition, wx.wxDefaultSize)
-    propSizer:Add(apply, 0, wx.wxALIGN_RIGHT + wx.wxALL, 5)
-
-    -- Event handler
-    propertiesPanel:Connect(applyId, wx.wxEVT_COMMAND_BUTTON_CLICKED, function()
+        local idMapping = {}
         for i, signal in ipairs(self.signals) do
-            local choice = idMapping[signal]
-            local selection = choice:GetStringSelection()
-            if (signal.slot == nil and selection ~= "") or (signal.slot and signal.slot.id ~= selection) then
-                signal:connect(self.controller:xcGetSlotById(selection))
-            elseif signal.slot and selection == "" then
-                signal.slot = nil
+            local label = wx.wxStaticText(propertiesPanel, wx.wxID_ANY, string.format("%s Action:", signal.id))
+            propSizer:Add(label, 0, wx.wxALIGN_LEFT + wx.wxALL, 5)
+            local choice = wx.wxChoice(propertiesPanel, wx.wxID_ANY, wx.wxDefaultPosition, wx.wxDefaultSize,
+                self.__type == "Trigger" and analogOptions or options)
+            idMapping[self.signals[i]] = choice
+            if self.signals[i].slot ~= nil then
+                choice:SetSelection(choice:FindString(self.signals[i].slot.id))
             end
+            propSizer:Add(choice, 1, wx.wxEXPAND + wx.wxALL, 5)
         end
-    end)
 
-    -- Refresh and return the layout
-    propertiesPanel:Layout()
-    propertiesPanel:Fit()
-    propertiesPanel:Refresh()
-    return propSizer
+        -- Add the apply button and the event handler 
+        propSizer:Add(0, 0)
+        local applyId = wx.wxNewId()
+        local apply = wx.wxButton(propertiesPanel, applyId, "Apply", wx.wxDefaultPosition, wx.wxDefaultSize)
+        propSizer:Add(apply, 0, wx.wxALIGN_RIGHT + wx.wxALL, 5)
+
+        -- Event handler
+        propertiesPanel:Connect(applyId, wx.wxEVT_COMMAND_BUTTON_CLICKED, function()
+            for i, signal in ipairs(self.signals) do
+                local choice = idMapping[signal]
+                local selection = choice:GetStringSelection()
+                if (signal.slot == nil and selection ~= "") or (signal.slot and signal.slot.id ~= selection) then
+                    signal:connect(self.controller:xcGetSlotById(selection))
+                elseif signal.slot and selection == "" then
+                    signal.slot = nil
+                end
+            end
+        end)
+
+        -- Refresh and return the layout
+        propertiesPanel:Layout()
+        propertiesPanel:Fit()
+        propertiesPanel:Refresh()
+        return propSizer
+    else
+        -- Disable config for an assigned shift button.
+        local label = wx.wxStaticText(propertiesPanel, wx.wxID_ANY, "This input is currently assigned as the shift button.")
+        propSizer:Add(0,0)
+        propSizer:Add(label, 0, wx.wxALIGN_LEFT + wx.wxALL, 5)
+        return propSizer
+    end
 end
 
+--- Object representing an analog pushbutton controller input.
 ---@class Trigger: Button 
 ---@field new function
 ---@field value number|nil
@@ -118,10 +134,10 @@ Trigger.__tostring = function(self)
     return string.format("Trigger: %s", self.id)
 end
 
---- Initialize a new `Trigger` (Derived from `Button`)
----@param controller Controller @A `Controller` instance
----@param id string @unique identifier for the `Trigger` object
----@return Trigger @the new `Trigger` instance
+--- Initialize a new Trigger instance.
+---@param controller Controller @A Controller instance
+---@param id string @unique identifier for the Trigger object
+---@return Trigger @the new Trigger instance
 function Trigger.new(controller, id)
     ---@class Trigger
     local self --[[@as Trigger]] = Button.new(controller, id)
@@ -133,7 +149,7 @@ function Trigger.new(controller, id)
     return self
 end
 
----@param self Trigger
+--- Retrieve the state of the input.
 function Trigger:getState()
     self.value = self.controller:xcGetRegValue(string.format("mcX360_LUA/%s", self.id))
     if type(self.value) ~= "number" then
@@ -155,8 +171,8 @@ function Trigger:getState()
     end
 end
 
----@param self Trigger
----@param func function
+--- Connect a Trigger's analog output to a function.
+---@param func function @The function to connect.
 function Trigger:connect(func)
     self.controller.isCorrectSelf(self) -- should raise an error if method has been called with dot notation
     self.controller.typeCheck({func}, {"function"}) -- should raise an error if any param is of the wrong type
