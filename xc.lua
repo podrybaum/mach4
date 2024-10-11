4f0500b76a3d5f909cbd4660401bb115a46ce290
podrybaum <podrybaum@gmail.com>
2024-10-11 07:30:56 -0500

    def profile

--- xc.lua
+++ xc.lua
@@ -24,6 +24,43 @@ end
 -- Global Mach4 instance
 inst = mc.mcGetInstance()
 
+if not mc.mcProfileExists(inst, "ControllerProfile-default") then
+    defaultProfile = [[[ControllerProfile-default]
+    xc.LTH_Y_Val.deadzone=10.000000
+    xc.RTH_X_Val.deadzone=10.000000
+    xc.RTH_Y_Val.deadzone=10.000000
+    xc.LTH_X_Val.deadzone=10.000000
+    xc.jogIncrement=0.100000
+    xc.logLevel=2.000000
+    xc.xYReversed=false
+    xc.frequency=4.000000
+    xc.simpleJogMapped=false
+    xc.profileName=default
+    xc.shiftButton=LTR_Val
+    xc.RTH_Y_Val.axis=2.000000
+    xc.RTH_Y_Val.inverted=false
+    xc.DPad_UP.Down.slot=xcJogUp
+    xc.DPad_UP.Up.slot=xcJogStopY
+    xc.DPad_DOWN.Down.slot=xcJogDown
+    xc.DPad_DOWN.Up.slot=xcJogStopY
+    xc.DPad_RIGHT.Down.slot=xcJogRight
+    xc.DPad_RIGHT.Up.slot=xcJogStopX
+    xc.DPad_LEFT.Down.slot=xcJogLeft
+    xc.DPad_LEFT.Up.slot=xcJogStopX
+    xc.DPad_UP.AltDown.slot=xcJogIncUp
+    xc.DPad_DOWN.AltDown.slot=xcJogIncDown
+    xc.DPad_RIGHT.AltDown.slot=xcJogIncRight
+    xc.DPad_LEFT.AltDown.slot=xcJogIncLeft
+    xc.Btn_B.Down.slot=E Stop Toggle
+    xc.Btn_RS.Down.slot=Enable Toggle
+    xc.Btn_X.Down.slot=XC Run Cycle Toggle
+    xc.Btn_BACK.AltDown.slot=Home All]]
+
+    local dir = mc.mcProfileGetName(inst)
+    local file = io.open(string.format("C:\\Mach4Hobby\\Profiles\\%s", dir), a)
+    file.write(defaultProfile)
+end
+
 --- Alias for a helpful one-liner.  If x ~= y, assign y to x.
 ---@param x any @a variable
 ---@param y any @a variable
