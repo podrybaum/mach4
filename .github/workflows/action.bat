2f205fa1b4c3efaca488273d3cd6c6ac2103ef99
podrybaum <podrybaum@gmail.com>
2024-10-24 15:29:43 -0500

    fucking chocolately

--- .github/workflows/action.bat
+++ .github/workflows/action.bat
@@ -1,20 +1,25 @@
 @echo off
 
-REM Install Lua 5.3 using Chocolatey
-choco install lua53 -y
+REM Download Lua 5.3 Windows binaries
+curl -L https://sourceforge.net/projects/luabinaries/files/5.3.5/Windows%20x64%20DLL/lua-5.3.5_Win64_bin.zip/download -o lua.zip
+
+REM Unzip Lua binaries
+powershell -command "Expand-Archive -Path lua.zip -DestinationPath ."
+
+REM Move Lua binaries to C:\Lua53 (create the directory if it doesn't exist)
+mkdir C:\Lua53
+move lua-5.3.5_Win64_bin\* C:\Lua53\
 
 REM Add Lua to the PATH
-set PATH=%PATH%;C:\ProgramData\chocolatey\lib\lua53\tools
-
-refreshenv
+set PATH=%PATH%;C:\Lua53
 
 REM Confirm Lua is in the PATH
-lua53 -v
+lua -v
 
 REM Update the package path for the Windows environment
 setlocal enabledelayedexpansion
 
-lua53 -e "package.cpath = package.cpath .. ';%CD%\\build\\?.dll'; require('mocks'); print('Lua environment set up')"
+lua -e "package.cpath = package.cpath .. ';%CD%\\build\\?.dll'; require('mocks'); print('Lua environment set up')"
 
 REM Change directory to the mach4 directory (adjust this path if needed)
 cd mach4
@@ -24,4 +29,4 @@ curl -L https://github.com/seaofvoices/darklua/releases/download/v0.14.0/darklua
 powershell -command "Expand-Archive -Path darklua.zip -DestinationPath ."
 
 REM Run the build script
-lua53 buildScript.lua
+lua buildScript.lua
