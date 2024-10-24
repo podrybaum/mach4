3014b6f3606071bf78861f38705d847934e5c83d
podrybaum <podrybaum@gmail.com>
2024-10-24 07:32:42 -0500

    should fix it

--- .github/workflows/action.sh
+++ .github/workflows/action.sh
@@ -1,4 +1,5 @@
 sudo apt install -y lua5.3
+sudo apt install -y libwxbase3.0-0v5 libwxgtk3.0-gtk3-0v5 wxlua
 cd mach4
 curl -L https://github.com/seaofvoices/darklua/releases/download/v0.14.0/darklua-linux-x86_64.zip -o darklua.zip
 unzip darklua.zip
