ea4c80ce3157c364ff5b1b9b78f99b574f0aef4c
podrybaum <podrybaum@gmail.com>
2024-10-24 07:18:49 -0500

    grrr

--- .github/workflows/action.sh
+++ .github/workflows/action.sh
@@ -1,9 +1,9 @@
-apt update
-apt install -y lua5.3
-ln -s /usr/bin/lua5.3 /usr/bin/lua
-git clone https://github.com/podrybaum/mach4.git
-cd mach4
-git checkout dev
-curl -L https://github.com/seaofvoices/darklua/releases/download/v0.14.0/darklua-linux-x86_64.zip -o darklua.zip
-unzip darklua.zip
-lua buildScript.lua
+sudo apt update
+sudo apt install -y lua5.3
+sudo ln -s /usr/bin/lua5.3 /usr/bin/lua
+sudo git clone https://github.com/podrybaum/mach4.git
+sudo cd mach4
+sudo git checkout dev
+sudo curl -L https://github.com/seaofvoices/darklua/releases/download/v0.14.0/darklua-linux-x86_64.zip -o darklua.zip
+sudo unzip darklua.zip
+sudo lua buildScript.lua
