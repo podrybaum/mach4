335c446f56612b8b71dd77ba00b03fa11f2c3016
podrybaum <podrybaum@gmail.com>
2024-10-24 07:34:39 -0500

    one more

--- .github/workflows/action.sh
+++ .github/workflows/action.sh
@@ -1,6 +1,20 @@
-sudo apt install -y lua5.3
-sudo apt install -y libwxbase3.0-0v5 libwxgtk3.0-gtk3-0v5 wxlua
-cd mach4
+#!/bin/bash
+
+# Install Lua 5.3 and other dependencies
+sudo apt install -y lua5.3 libgtk2.0-dev libwxgtk3.0-gtk3-dev liblua5.3-dev unzip curl build-essential
+
+# Clone wxLua and build it
+git clone https://github.com/pkulchenko/wxlua.git
+cd wxlua/wxLua
+make
+sudo make install
+
+# Navigate back to your Mach4 directory
+cd ../../mach4
+
+# Download and unzip darklua
 curl -L https://github.com/seaofvoices/darklua/releases/download/v0.14.0/darklua-linux-x86_64.zip -o darklua.zip
 unzip darklua.zip
+
+# Run your build script
 lua buildScript.lua
